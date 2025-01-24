967c6f656b6b0152b7d2df34f1ce3e61ee02e0db
AlvaroRCardoso <alvaro.cardoso@universo.univates.br>
2025-01-24 11:55:08 -0300

    COMMIT

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // ou false, dependendo da sua preferência
 };
 
 export default nextConfig;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div>oLÁ MUNDO!</div>;
+  return <div>OLÁ MUNDO!</div>;
 }
